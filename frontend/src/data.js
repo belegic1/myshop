7cab23e0eac62539e2d937102d3539b52502ff8a
belegic1 <www.belegic@gmail.com>
2020-12-20 16:43:11 +0100

    'initializing'

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id: 1,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p1.jpg'
+        },
+        {
+            _id: 2,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p2.jpg'
+        },
+        {
+            _id: 3,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p3.jpg'
+        },
+        {
+            _id: 4,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p4.jpg'
+        },
+        {
+            _id: 5,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p5.jpg'
+        },
+        {
+            _id: 6,
+            name: 'Slim Shirt' ,
+            category: 'Shirt',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 19,
+            price: 120,
+            image: '/images/p6.jpg'
+        },
+    ]
+}
+
+
+export default data
